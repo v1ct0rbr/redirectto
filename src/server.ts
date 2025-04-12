beeb21897ac4ce5b915ccad8759428a42b1a6f2a
Victor Queiroga <victorqueiroga.jp@gmail.com>
2025-04-12 19:18:43 -0300

    refactor server.ts to create an app instance for each port and improve request handling

--- src/server.ts
+++ src/server.ts
@@ -3,35 +3,48 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const app = express();
+
 const ports = (process.env.INTERNAL_PORTS || '3000,3001').split(',').map(Number);
 const redirectUrl = process.env.REDIRECT_URL || 'http://localhost:3000';
 const redirectType = process.env.REDIRECT_TYPE as string;
 const copyOriginalPart = process.env.COPY_ORIGINAL_PART === 'true';
 
-
-app.use(async (req, res) => {
-    const fullOriginalUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-    console.log(`Original URL: ${fullOriginalUrl}`);
-
-    const originalUrlParts = req.originalUrl;
-    const finalUrl = redirectUrl + (copyOriginalPart ? originalUrlParts.replace(",", "/") : '');
-    console.log(`Redirecting to: ${finalUrl}`);
-
-    let redirectTypeNumber = parseInt(redirectType, 10);
-    if (isNaN(redirectTypeNumber) || redirectTypeNumber < 300 || redirectTypeNumber > 399) {
-
-        console.error(`Invalid redirect type: ${redirectType}. Defaulting to 301.`);
-        redirectTypeNumber = 301; // Default to 301 if invalid
-    }
-    res.redirect(redirectTypeNumber, finalUrl);
-});
-
-ports.forEach(port => {
+for (const port of ports) {
+    // ciar uma app para cada porta
+    const app = express();
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
     });
-});
+    app.use((req, res) => {
+        const fullOriginalUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+        const ipAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+        console.log("----------------------------------------");
+        console.log(`IP Address: ${ipAddress}`);
+        console.log(`Request Protocol: ${req.protocol}`);
+        console.log(`Url original completa: ${fullOriginalUrl}`);
+
+
+        const originalUrlParts = req.originalUrl;
+        const finalUrl = redirectUrl + (copyOriginalPart ? originalUrlParts.replace(",", "/") : '');
+        console.log(`Redirecting to: ${finalUrl}`);
+
+
+        let redirectTypeNumber = parseInt(redirectType, 10);
+        if (isNaN(redirectTypeNumber) || redirectTypeNumber < 300 || redirectTypeNumber > 399) {
+
+            console.error(`Invalid redirect type: ${redirectType}. Defaulting to 301.`);
+            redirectTypeNumber = 301; // Default to 301 if invalid
+        }
+        console.log("----------------------------------------");
+        res.redirect(redirectTypeNumber, finalUrl);
+    });
+    console.log(`Server is running on port ${port}`);
+
+
+}
+
+
+
 
 
 
