be9efe2b27f05942155f7a4e2781b760723af338
Victor Queiroga <victorqueiroga.jp@gmail.com>
2025-04-12 18:46:41 -0300

    refactor server.ts to support multiple internal ports and clean up code formatting

--- src/server.ts
+++ src/server.ts
@@ -4,33 +4,35 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.INTERNAL_PORT || 3000;
+const ports = (process.env.INTERNAL_PORTS || '3000,3001').split(',').map(Number);
 const redirectUrl = process.env.REDIRECT_URL || 'http://localhost:3000';
 const redirectType = process.env.REDIRECT_TYPE as string;
 const copyOriginalPart = process.env.COPY_ORIGINAL_PART === 'true';
 
+
 app.use(async (req, res) => {
     const fullOriginalUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
     console.log(`Original URL: ${fullOriginalUrl}`);
-   
+
     const originalUrlParts = req.originalUrl;
-    const finalUrl = redirectUrl + (copyOriginalPart ? originalUrlParts.replace(",","/") : '');
+    const finalUrl = redirectUrl + (copyOriginalPart ? originalUrlParts.replace(",", "/") : '');
     console.log(`Redirecting to: ${finalUrl}`);
-  
+
     let redirectTypeNumber = parseInt(redirectType, 10);
     if (isNaN(redirectTypeNumber) || redirectTypeNumber < 300 || redirectTypeNumber > 399) {
 
         console.error(`Invalid redirect type: ${redirectType}. Defaulting to 301.`);
         redirectTypeNumber = 301; // Default to 301 if invalid
     }
-
     res.redirect(redirectTypeNumber, finalUrl);
-   
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-   
+ports.forEach(port => {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
 });
+
+
 
 
